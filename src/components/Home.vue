5fec4e3fce45cbab9957f101709a21fa83e19ad5
sfynath <sharonfnathaly@gmail.com>
2025-11-18 17:04:37 +0700

    initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,67 @@
+<script setup>
+  function getFile(){
+    document.getElementById("getFile").click();
+  }
+
+  
+</script>
+
+<template>
+  <div class="home">
+    <div id="homeRightSide">
+      <h1>Deepfake Video Detector Tool</h1>
+
+      <div class="inputFileButton">
+        <button class="theFileButton" @click="getFile()">Upload your video here!</button>
+        <input type="file" accept="video/*" id="getFile" style="display: none;">
+      </div>
+    </div>
+    <div class="homeVideo">
+      <video class="theHomeVideo" autoplay controls loop>
+        <source src="../vid/01_15__outside_talking_still_laughing__02HILKYO.mp4" type="video/mp4">
+        Your browser does not support the video tag.
+      </video>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: bold;
+  font-size: 2.6rem;
+  position: absolute;
+  /* top: -10px; */
+  color: #ffffff;
+}
+
+.theFileButton {
+  color: rgb(0, 0, 0);
+  padding: 15px;
+  top: 10px;
+  border-radius: 50px;
+  background-color: #6b75ff;
+  transition: 0.7s;
+  cursor: pointer;
+}
+
+.theFileButton:hover {
+  background-color: #3a44c9;
+  color: rgb(255, 255, 255);
+}
+
+.homeVideo .theHomeVideo {
+    position: absolute;
+    /* z-index:-1; */
+    top: 10;
+    left: 5;
+    width: 40vw; 
+    /* height: 100vh; */
+    border-radius: 60px;
+}
+
+#homeRightSide {
+  position: fixed;
+  right: 10vw;
+}
+
+</style>
---
+++ src/components/navbar.vue
@@ -0,0 +1,10 @@
+<template>
+    <div id="allnavbar">
+        <img class="logo" src="" alt="">
+        <div>
+            
+        </div>
+    </div>
+
+
+</template>
